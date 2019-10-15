65f64768a6e82a5ca3e26f5f1f90b3f44397020b
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-15 21:30:47 +0630

    Install vue-router module and Implement the Route

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,12 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <!-- <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a> -->
+            <router-link class="nav-link" :to="'/'">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <!-- <a class="nav-link" href="/contact">Link</a> -->
+            <router-link class="nav-link" :to="'/contact'">Contact</router-link>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -36,18 +38,20 @@
       </div>
     </nav>
     <img src="./assets/logo.png">
-    <posts-example></posts-example>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
 import PostsExample from './components/Posts'
+import Contact from './components/Contact'
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    PostsExample
+    PostsExample,
+    Contact
   }
 }
 </script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>CONTACT US</h2>
+    </div>
+</template>
