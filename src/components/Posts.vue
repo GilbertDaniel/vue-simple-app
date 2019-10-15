3466e7e2aea9ec8a651bfbd700497c997cef859f
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-15 20:38:54 +0630

    Install axios npm module and get post data from the api

---
+++ src/components/Posts.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <table class="table mt-4">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">User ID</th>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Body</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                <th scope="row">{{post.userId}}</th>
+                <td>{{post.id}}</td>
+                <td>{{post.title}}</td>
+                <td>{{post.body}}</td>
+                </tr>
+            </tbody>
+        </table>
+
+            
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name:'PostsExample',
+    data(){
+        return {
+            posts: []
+        }
+    }, 
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(posts => {
+            this.posts = posts.data
+        });
+        console.log("It's working")
+    }
+}
+</script>
+<style>
+
+</style>
